df768edd5393c905d6e574c8032b1487b727353c
Samsul Hadi <samsulhadikediri@gmail.com>
2021-08-09 08:29:50 +0700

    Initial Commit

---
+++ AppNameView/AppNameViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNameViewApp.swift
+//  AppNameView
+//
+//  Created by Samsul Hadi on 09/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AppNameViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppNameView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppNameView
+//
+//  Created by Samsul Hadi on 09/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
